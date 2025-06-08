dc206317a466c96c3a9b0b86dcdda4459659709b
Nafea Alhelali <warrior259@gmail.com>
2025-06-08 22:51:48 +0300

    feat: set up mqtt connection and client

--- main/firmware.c
+++ main/firmware.c
@@ -8,9 +8,29 @@
 #include "esp_event.h"
 #include "esp_netif.h"
 #include "esp_log.h"
+#include "mqtt_client.h"
+#include "protocol_examples_common.h"
 
 
 static const char *TAG = "firmware";
+
+esp_mqtt_client_handle_t setup_mqtt_client(void)
+{
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+    * Read "Establishing Wi-Fi or Ethernet Connection" section in
+    * examples/protocols/README.md for more information about this function.
+    */
+    ESP_ERROR_CHECK(example_connect());
+
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = CONFIG_BROKER_URL,
+    };
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+
+    esp_mqtt_client_start(client);
+
+    return client;
+}
 
 void app_main(void)
 {
@@ -21,6 +41,14 @@ void app_main(void)
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_mqtt_client_handle_t client = setup_mqtt_client();
+    if (client == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to create MQTT client");
+        esp_restart();
+    }
+    ESP_LOGI(TAG, "MQTT client created successfully");
 
     while (1)
     {
