2cd664269b5bc26b90fd20d173012ee5eff81bdd
Nafea Alhelali <warrior259@gmail.com>
2025-06-08 22:32:18 +0300

    feat: init esp-idf project

---
+++ main/firmware.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_log.h"
+
+
+static const char *TAG = "firmware";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    while (1)
+    {
+        ESP_LOGI(TAG, "Looping ...");
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    }
+}
